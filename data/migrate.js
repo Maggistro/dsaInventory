8229f403a2d5ddd083c52c260b1d9f3812fce24a
maggistro <maggistro@googlemail.com>
2024-07-31 12:23:20 +0200

    Add sqlite

---
+++ data/migrate.js
@@ -0,0 +1,24 @@
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite';
+import fs from 'fs'
+
+open({
+    filename: 'data/db.sqlite',
+    driver: sqlite3.Database,
+}).then(async db => {
+    const migrations = JSON.parse(fs.readFileSync('data/migrations.json'));
+
+    await db.run("CREATE TABLE IF NOT EXISTS migrations (id varchar(255), date int)");
+    const migrationList = await db.all("SELECT * FROM migrations ORDER BY id ASC");
+
+    for(let migrationId=0; migrationId<Object.keys(migrations).length; migrationId++) {
+        if (migrationList.find(row => row.id === migrationId)) {
+            continue;
+        }
+
+        await Promise.all(migrations[migrationId].up.map(async instruction => {
+            await db.run(instruction);
+        }))
+        db.run(`INSERT INTO migrations VALUES (${migrationId}, ${Date.now()})`);
+    }
+})
