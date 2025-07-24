f2cbb290fb6d4a0a346f790c62bbc65baeaa60af
maggistro <maggistro@googlemail.com>
2025-07-24 17:30:45 +0200

    Add import

--- commands/previousPage.js
+++ commands/previousPage.js
@@ -2,6 +2,7 @@ import { InteractionResponseType, InteractionResponseFlags } from 'discord-inter
 import { getInventory } from '../data/inventory.js';
 import { buildTable } from '../format/buildTable.js';
 import { ITEM_LIMIT } from './listItems.js';
+import { NEXT_PAGE } from './nextPage.js';
 
 const PREVIOUS_PAGE = 'previouspage';
 
